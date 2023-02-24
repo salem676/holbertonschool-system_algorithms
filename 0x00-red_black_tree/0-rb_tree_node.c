587d9b0471a3703c7aa0717aadacec0c5125fb20
salem676 <echoesofeternal@gmail.com>
2023-02-24 14:17:09 -0600

    refresh

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -1,75 +1,23 @@
 #include "rb_trees.h"
-
 /**
- * rb_tree_node - creates a Red-Black Tree node
- * @parent: pointer to parent node
- * @value: value to put in the new node
- * @color: color of the node
- * Return: pointer to new node, or NULL on failure
- */
+* rb_tree_node -  creates a Red-Black Tree node
+* @parent: Parent
+* @value: New node value
+* @color: Node Color
+* Return: A new_node node on success, NULL otherwise
+*/
 rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 {
-	rb_tree_t *new;
+	rb_tree_t *new_node = NULL;
 
-	new = malloc(sizeof(rb_tree_t));
-	if (!new)
+	new_node = malloc(sizeof(rb_tree_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	new->parent = parent;
-	new->n = value;
-	new->color = color;
-	new->left = NULL;
-	new->right = NULL;
-
-	return (new);
-}
-
-/**
- * rb_rotate_left - rotates subtree to left
- *
- * @tree: root node
- * @x: node to rotate
- */
-void rb_rotate_left(rb_tree_t **tree, rb_tree_t *x)
-{
-	rb_tree_t *y = NULL;
-
-	y = x->right;
-	x->right = y->left;
-	if (y->left)
-		y->left->parent = x;
-	y->parent = x->parent;
-	if (x->parent == NULL)
-		*tree = y;
-	else if (x == x->parent->left)
-		x->parent->left = y;
-	else
-		x->parent->right = y;
-	y->left = x;
-	x->parent = y;
-}
-
-/**
- * rb_rotate_right - rotates subtree to right
- *
- * @tree: root node
- * @y: node to rotate
- */
-void rb_rotate_right(rb_tree_t **tree, rb_tree_t *y)
-{
-	rb_tree_t *x = NULL;
-
-	x = y->left;
-	y->left = x->right;
-	if (x->right)
-		x->right->parent = y;
-	x->parent = y->parent;
-	if (y->parent == NULL)
-		*tree = x;
-	else if (y == y->parent->left)
-		y->parent->left = x;
-	else
-		y->parent->right = x;
-	x->right = y;
-	y->parent = x;
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->color = color;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
 }
